8a90a2d65ef3164901e09f4234b83216a81da48a
Kenesary Kalmat <k.kalmat@a1qa.com>
2025-04-21 15:27:32 +0500

    Updated

--- tests/support/hooks.js
+++ tests/support/hooks.js
@@ -1,19 +1,24 @@
-const { BeforeAll, AfterAll, Before } = require('@cucumber/cucumber');
+const { BeforeAll, AfterAll, Before, After } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 
 let browserInstance;
 
 BeforeAll(async () => {
-  console.log('[BeforeAll] Launching browser...');
-  browserInstance = await chromium.launch({ headless: true });
+    console.log('[BeforeAll] Launching browser...');
+    browserInstance = await chromium.launch({ headless: true });
 });
 
 AfterAll(async () => {
-  console.log('[AfterAll] Closing browser...');
-  await browserInstance.close();
+    console.log('[AfterAll] Closing browser...');
+    await browserInstance.close();
 });
 
 Before(async function () {
-  const context = await browserInstance.newContext();
-  this.page = await context.newPage();
+    this.context = await browserInstance.newContext();
+    this.page = await this.context.newPage();
 });
+
+After(async function () {
+    await this.page.close();
+    await this.context.close();
+})
